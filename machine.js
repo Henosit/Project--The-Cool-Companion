5ee6db986238f82d29a9d2389805a6bce0784c71
Henosit <chenmoasis@gmail.com>
2023-04-23 18:25:51 +0300

    this is a test commit

---
+++ machine.js
@@ -0,0 +1,9 @@
+import { createMachine } from 'xstate';
+const Smart_Fridge = createMachine({
+    /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgwEMBbMAeQDN8QAHLWASwBcGsNqAPRAWgDZ0Anj17I0mEuQoA6BhmYNCAGwDKTQkzDU6jFm06IALACZBiABwBGKQYCcdm0YAMZ3gHYjFiwFZRooA */
+    id: 'The Cool Companion',
+    initial: 'initial',
+    states: {
+        initial: {},
+    }
+});
