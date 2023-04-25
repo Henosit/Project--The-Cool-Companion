500e806a3868972e4f617101b8b4a0719f1b7176
Aiden Kaminsky <AidenK@HIT>
2023-04-25 12:52:06 +0300

    added actions and temp, TODO

--- machine.js
+++ machine.js
@@ -1,57 +1,98 @@
 import { createMachine } from 'xstate';
 
-const SmartFridge = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBUAWYDCB7LAbbAtgA4CGAdgJZZkB0AYgE5hgBeNWAZhwMQYDyAOQEBRDMgD6yPuIAKfAOrCASgG0ADAF1EoIllgUALlTLaQAD0QBGAKwAWGgA4HATjUAmAMxq1H25bWWAGwANCAAnogAtLbWbjSWCTaWAOxOgc7JQQC+WaFomDj4WMTkxvRMrOy0FBC4YNxS0gASAJIA4k2SwgCyMsoAgsgAqkrC6lpIILr6RtSmFggJHvEO1h6BvrYObrbOlh6hEQiRbi40yYFqTh7Orv43Hjl56Nh4hKSU1OXMbF8cFSwKGQoNxRv0MJ1kD0+kpBiMxppTNNDMZ5lZLA4aGpkrcHF5vA5vM4HIcom41PZbG5ktZrM4PMlklTrMkniB8q8iiVPrQACI4Bg0ADGuD0kG4fD6AnEvL4fFUiMmyNmJkmCxp8Qu1kC9y2eLUIXCiA8lnOdipnkuHms-lsj1y7JehXepS+-KwgqwRDAZHFGAAMnwAMrCGVyhUTHR6FFzNWITI0DweU6WWxp3ZXG2kxZXLHOPwZO4+G05B1kLAQOCmDnO4ofVFK6MqtHHSxuOLttwJNZ25PWdzZyKBanxRLDzOMpNsmtvOuu2iMH5IpsN0ALSLJZaMi7DuzOdsJZyDtbOGg67vpBmeNbTp2z7llReVTgcZczVfmKKBGidttUwIxMOQQ2IOMRxIkSSpA46SZIEt4FPe9ZfE+vyqlG76xmuZLWOcjKBLuuwHhiBxGscDLJD+lxOHsHjQVS8Gci6PLfM+1S1GAb4xmhn4IO2P7tkmTL9oyTgpIOOz2A4fhuMOm5uK4MS2AxtYPshAJVDQ-w-ECUCcc2cYIEmW4UqmOqbgkw7idaNDCfuDjET4LgOMpiHzjQ7oMHpH7rky-FdjYvhJm4-ZuMeOFCdY2xQRiyTuC5XJIXyArCqKsCQF5mE8Sc36-gFvbBe4oWkQBNB+Ph0E2PZMVuPFTFlB57Der6EAZdxCxdt++ZBBkjL5k4kXZpuiZqNqPhBb4fWllkQA */
-  id: 'TheCoolCompanion',
-  initial: 'off',
-  type: 'parallel',
-  states: {
-    Freez: {
-      initial: 'off',
-      states: {
-        off: {
-          on: {
-            CONNECT_TO_POWER: "on",
-          },
-        },
-        on: {
-          initial: 'idle',
-          states: {
-            idle: {
-              on: {
-                TOO_HIGH_TEMPERATURE: {
-                    target:"freezing"
-                }
-              },
-            },
-
-            freezing: {
-              on: {
-                REACH_TEMPERATURE: {
-                    target: "idle"
-                }
-              },
+const SmartFridge = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5QBUAWYDCB7LAbbAtgA4CGAdgJZZkB0AYgE5hgBeNWAZhwMQYDyAOQEBRDMgD6yPuIAKfAOrCASgG0ADAF1EoIllgUALlTLaQAD0QAmAJwAWGpYDsaxzYCsANjdvLARkcANCAAnoi2jjTWNgDM0bYAHI5x0XbRAL5pQWiYOPhYxOTG9Eys7LQUELhg3FLSABIAkgDidZLCALIyygCCyACqSsLqWkgguvpG1KYWCL62HpHR8T6+8UuOnkGhCG5q1jQeHrbW1v7We7aWHhlZ6Nh4hKSU1MXMbC8cJSwUZFDcg90MK1kB0ukpegMhppTONDMZpmFfAsPGo1PNHFF4lEXFtENFfG5Ir41G5rEtVmoselMiBsvc8gVnrQACI4Bg0ADGuD0kG4fC6AnEzL4fFU0NGsMmJlGM1W8SJCUctiR8TlmxCiHiaksNBS8UO+v8ag8rhutLuuUehRerKw7KwRDAZF5GAAMnwAMrCIUisUjHR6OFTGWIXzWCKq+JY8IokmWaK4hBKtQOaxHVZRDa+JwZGlkLAQOCmOmW-JPeESwNShEIAC0S11qJS-n8jgJfjcidr2YiLmNh1RkcSZpLDzL1tojDeMKrFdAM32HhOKPillXa-Cyy7KRobmchw8yxSS8cjhHFrHjKKU9KnA4M4mc-MiD3B2XlLXlg3ji3GqTC2VY55lsSkojcaJrhpUcGXLF4b3eaUA0fYN50QQ4aDUfFrDcKMrniZVHA8RMv3lZUSVVDEMTDc8ckvWDJy+MoaAqKoHyDRDn1mOYMKwnDV0PAiiL-OIUxw2xvBA3wzg8eMaPpK0mVeW9aE+N4figNjqxDBBNxoLVbAMuJDKjXxE1saJRPjEkLNiVVVjk0srxtNlNKfGZLEJDw5X1DxojcbMowTP8vB1DEjhw6I2zmDEHLoicaFtdkuR5CBXJQzikgObzDj8gK1kTJUFki8MfC8cITjcWKYPixL2EdZ1UsrZCOJmCDfBoECjhcWJCPOSwCoSPTKNKtxyuw3M0iAA */
+    id: 'TheCoolCompanion',
+    initial: 'off',
+    type: 'parallel',
+    context: {
+      temperature: 25,
+      interval: null
+    },
+    states: {
+      Freez: {
+        initial: 'off',
+        states: {
+          off: {
+            on: {
+              CONNECT_TO_POWER: {
+                target: 'on',
+                actions: ['setTemperature', 'startInterval']
+              }
             }
           },
-        },
-      },
-    },
-    Door: {
-      initial: 'closed',
-      states: {
-        closed: {
           on: {
-            OPEN_DOOR: 'opened',
-          },
-        },
-        opened: {
-          on: {
-            CLOSE_DOOR: 'closed',
-          },
-        },
+            initial: 'idle',
+            states: {
+              idle: {
+                entry: 'setTemperature',
+                on: {
+                  TOO_HIGH_TEMPERATURE: {
+                    target: 'freezing',
+                    cond: 'isBelowThreshold'
+                  }
+                }
+              },
+              freezing: {
+                on: {
+                  REACH_TEMPERATURE: {
+                    target: 'idle',
+                    actions: ['setTemperature']
+                  }
+                }
+              }
+            }
+          }
+        }
       },
-    },
+      Door: {
+        initial: 'closed',
+        states: {
+          closed: {
+            on: {
+              OPEN_DOOR: 'opened'
+            }
+          },
+          opened: {
+            on: {
+              CLOSE_DOOR: 'closed'
+            }
+          }
+        }
+      }
+    }
   },
-});
+  {
+    actions: {
+      setTemperature: (context) => {
+        if (context.temperature >= -4) {
+          context.temperature -= 1;
+        } else {
+          context.temperature += 1;
+        }
+      },
+      startInterval: (context) => {
+        if (context.interval) {
+          clearInterval(context.interval);
+        }
+        context.interval = setInterval(() => {
+          context.temperature += 1;
+        }, 1000);
+      }
+    },
+    guards: {
+      isBelowThreshold: (context) => {
+        return context.temperature <= -4;
+      }
+    },
+    on: {
+      // Update temperature when it changes
+      UPDATE_TEMPERATURE: {
+        actions: ['setTemperature']
+      }
+    }
+  }
+);
 
 export default SmartFridge;
