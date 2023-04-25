21d76db37f6bc2f34ce1262a7082b8768e408fb1
Aiden Kaminsky <AidenK@HIT>
2023-04-25 11:25:20 +0300

    added targets in on state

--- machine.js
+++ machine.js
@@ -1,7 +1,7 @@
 import { createMachine } from 'xstate';
 
 const SmartFridge = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBUAWYDCB7LAbbAtgA4CGAdgJZZkB0AYgE5hgBeNWAZhwMQYDyAOQEBRDMgD6yPuIAKfAOrCASgG0ADAF1EoIllgUALlTLaQAD0QAmAMw1r96wBYAjAHYAnNYCsADkfu1VwAaEABPRABaF2caADZ41zVnS0t-NUsvVwBfLJC0TBx8LGJyY3omVnZaCghcMG4AZWQAQSUJOiVhYQAtAEkBAHF1LSQQXX0jalMLBAjnNTUaZ0cfQIDXNy9vRxDw2edYxxpHS1cvS3j3eZ8vLxy89Gw8QlJKanLmNneOCpYKMig3E6zQwAAlJMIALIyZTNZAAVU6w1M40MxmmiB8sRoax8rg2akct3csXcu0iByOJzOF1J11u9xA+SeRRKb1ojE+VRosAMWCInNY-0BUmkABkFBDobCEUjNCi9GipqMZtY1O47LFUp5bmpzucdmEKYdjqdzpd6XdckzHoUXqV3gARHAMGgAY1wekg3D4MIE4kdfD4qnlo1RkxMKopMUJmVWnlO1h8zh8lnJCGcvlN82cyxclmc7kc1kZzLtxVeZWdWFd-LAZG9GAlDWEAaDIZGOkVEYx+yOPh87nONNOyVchr2NiOrligSx7mTAW8OWtZCwEDgpjLzwrDsjXYm6Kjs1SR2Wqw8gU23li6Yivmx8ViiS8RJJ82y1u3rMr70FLAVQ9lVAGY5hJJYVjWK8zhvO9-A1A5nySJw50JUtbR3Nkyn-dguEApV93MSJwIWUisTxeMhzg3MlgSeYMksPUk1idCCkw38OV+Kp8J7Y85hWCCL3Wa8vFvI1ZnsLxjnOaxnzcWciRLL8MJ-PcPkqd4ajqHijxAyIvAQvwAlSaxLCLRxCTvVJXBoIcbAMnwk0cN9PweNjVPZdSvloH5PmFHTgKIjNUhxPVx3Mp8ZLvPwaEHIsL1k9wF1k1iWXtTycPeXl+X-fyw27XSgucUy7D8awrjcFwLLEvYIms2yZIcpzHFnJS3LS3dPOrBgAsI0DC2xUikmiBZnzOdNElsgsFhcNRZMY4rUvLLCnRdd1PVgSBet7fifFChZllzUb8S8dMDik8LPCxObYmsVxUyW9i1O69giHrLb8qAvqrGSY53A2ONkkzE5rDO2ILvg5ibruzwVyyIA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBUAWYDCB7LAbbAtgA4CGAdgJZZkB0AYgE5hgBeNWAZhwMQYDyAOQEBRDMgD6yPuIAKfAOrCASgG0ADAF1EoIllgUALlTLaQAD0QBGAKwAWGgA4HATjUAmAMxq1H25bWWAGwANCAAnogAtLbWbjSWCTaWAOxOgc7JQQC+WaFomDj4WMTkxvRMrOy0FBC4YNxS0gASAJIA4k2SwgCyMsoAgsgAqkrC6lpIILr6RtSmFggJHvEO1h6BvrYObrbOlh6hEQiRbi40yYFqTh7Orv43Hjl56Nh4hKSU1OXMbF8cFSwKGQoNxRv0MJ1kD0+kpBiMxppTNNDMZ5lZLA4aGpkrcHF5vA5vM4HIcom41PZbG5ktZrM4PMlklTrMkniB8q8iiVPrQACI4Bg0ADGuD0kG4fD6AnEvL4fFUiMmyNmJkmCxp8Qu1kC9y2eLUIXCiA8lnOdipnkuHms-lsj1y7JehXepS+-KwgqwRDAZHFGAAMnwAMrCGVyhUTHR6FFzNWITI0DweU6WWxp3ZXG2kxZXLHOPwZO4+G05B1kLAQOCmDnO4ofVFK6MqtHHSxuOLttwJNZ25PWdzZyKBanxRLDzOMpNsmtvOuu2iMH5IpsN0ALSLJZaMi7DuzOdsJZyDtbOGg67vpBmeNbTp2z7llReVTgcZczVfmKKBGidttUwIxMOQQ2IOMRxIkSSpA46SZIEt4FPe9ZfE+vyqlG76xmuZLWOcjKBLuuwHhiBxGscDLJD+lxOHsHjQVS8Gci6PLfM+1S1GAb4xmhn4IO2P7tkmTL9oyTgpIOOz2A4fhuMOm5uK4MS2AxtYPshAJVDQ-w-ECUCcc2cYIEmW4UqmOqbgkw7idaNDCfuDjET4LgOMpiHzjQ7oMHpH7rky-FdjYvhJm4-ZuMeOFCdY2xQRiyTuC5XJIXyArCqKsCQF5mE8Sc36-gFvbBe4oWkQBNB+Ph0E2PZMVuPFTFlB57Der6EAZdxCxdt++ZBBkjL5k4kXZpuiZqNqPhBb4fWllkQA */
   id: 'TheCoolCompanion',
   initial: 'off',
   type: 'parallel',
@@ -19,19 +19,19 @@ const SmartFridge = createMachine({
           states: {
             idle: {
               on: {
-                START_FREEZING: "freezing",
+                TOO_HIGH_TEMPERATURE: {
+                    target:"freezing"
+                }
               },
             },
+
             freezing: {
               on: {
-                REACH_TEMPERATURE: "stopFreezing",
+                REACH_TEMPERATURE: {
+                    target: "idle"
+                }
               },
-            },
-            stopFreezing: {
-              on: {
-                TOO_LOW_TEMPERATURE: "freezing",
-              },
-            },
+            }
           },
         },
       },
