4eed6db95db839e279461e007ee2342f2fcb0847
Aiden Kaminsky <AidenK@HIT>
2023-05-17 10:11:56 +0300

    added time transitions btwn light, doror and alarm

--- machine.js
+++ machine.js
@@ -1,166 +1,181 @@
-import { createMachine } from 'xstate';
-
-const SmartFridge = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBUAWYAEBhA9jgNtjgLYAOAhgHYCWOlAdAEpgBmATtVNW+QC45t6AeRYsAxLkqUwAY14Z+GAAo4A7mDYBtAAwBdRKFI5Y1XrUoGQAD0QAmAIy36AZgAcAdm3P3918-sArAG2ADQgAJ6IALQAbDH0Ae4B2n72AJzaMbbuaTEAvnlhaJi4BERkVOZMrBxcPPyCQgyl+Br0LdSUUGLIYGQafACubJjM5DLoEDr6SCBGJmZ0ljYIUYn02rkx9s4ALK7atq5+u2GRCDF7LsHBaYFxfu4FRehEZbgVNHTV7JzcfAJhM08K1BB0umIAGIAQQAqgAZZDTSzzUzmZaIK7uWxpDJuTwBVy5WynCJ2Bz0A4xO7pWyZHKuALPEDFN6ED4UL4MZi-OoAxrAghtcHdACSMkw0JkMkGxEG+D4kGRs1RiwssxWUScROSuUcaV29i87kuZ0Qu1s8XcrnsuxifgCgWt2l2zNZLXKnKqPNq-waQPaILaoogrR6fVIA14w0wyDwGAAEpxUMrDMY0UsNdFds5KfjLvZ3DlEq5dqTzgEyxtcptqTFMoS3a8PRzKt8fX96oCmoGhYIACI1YxmCHizAAWTA+F4Sr0KPTaoxCHS9DSHh8lwJRKy5cxnhcfm0iUCR0NTMKLObIM9be5NU7-IDLTag-Yw863RhCKRc5VC-RWYIM4zjxEEjjUgE-i2AEZpAdo7guOkRYFtk-jOE2JTXq2XI-L6XYCvQkIjGAABebREWApEflCcKIqmcz-pmoArP49D2lS1JHIyBqMrBjJOGk2TaMJLqJCaroXu6WEkF67b3ny-o9hRpHkcRJHUWOGBSjKcoKjOUy-mmCwAcxmL0BaJKGoJxwBAaa7uLBRoWquQkibsYkxBJLyYe8Mm3rhD6KQwylkYIIXUb0-T1DGGBjBMs4zEZGbqqZqxpLmWQ8YWDi7O4zgGo50GuC5nhuR5XmXj57J+ThHYKd2wVqcGoZgOGUVDCMGBxjgibJvRqomdY0SltcxwpPB9i2keoRksugQBGxRYiaJyEVVJvmfN68l+g1hFNQOQ6wCOn60T+iUMcZTFDUBzjaGxdxEu5UEwbNaS2YtpXCe5q0YWyN61dt+EBiFL6HcdYiaZO04JfOl0pddlouNNlYWjZPEveca53YJn0reJv0tjVW28jtBHwsmvCwPQ8KigA4gmyAAMoAPpCJCkJiNGbCUBgdAYPgFOwP1jHw5qOzxNoRq2cJeyFs4tgzectoxAtuLWt49rCb4rgE9Jm1ySTQM9uTUCoJT1N0wzLNCAAcj0ww8yILD84Lwtw0uUQ7Nqmz+IWrj2okMSOfWaQbJumRHktZa6xtsl3obj49ozOCDJQEBaQqbDEPQjNCLCNv9qzdtWEdir0OQLAzmwAAUyTaAAlD0V6x-5dWkwGyep+n0KZ9nuf54Xttu8lS4BDECGHFB9ZZBrziOfs9hhxZwTUpsSQx9V+sMP2eCCDI+DGJAYhCJGPM7wIw+LoBWr2PEdxvdS3gHPs2KOffSP+8kOTge4a3N5vcd6Dnz3gfWAR8S68DLhXKu1d7CN3WgA-ywD6D70PgZc6A0rorFyk4NwLp9gR08tSWCdI1wbENLlNIPhHSXD-lVf6VRkE4FPkfLAoDMDAMvoNMWuRzJjxzK4Ww8tb6eUVnYF0od9jj12BkHEhodaSX-gw74TCWEQDEBAqBlcNCwPgUo7CjDd70GYWAaQ6DYYj0ArdBI1IcwOB2DaVIjlPC5lxLiGRFpfBxA3so7eRiTFmI0aXGc5dtE1zrno+hBiVH+LUVwrBiBx7FWtAIvwjJDiv1mukd6v8ixP0lu5BRF5KA4AgHASwCDfEWKvqlKIdpcz8LHr-Y06R0qwS1IaBIwE7HuUCHaE0Pjonxzwvyap3DsyBBse5aRLTcRz1mp7SsGxKy3UyOBXK6FFFRKJgbEZilRBjISasHEoFvAkkrBNUsGNohxASEkFI-gMhZByPkLZf0hkBXqgKQ5otoggQQo0mZ3hWnzPOHaSk+wx6WlsiaI0KtBk7OGYFXaz42A-I9tkUONp0jAQmlNa5y4rjyyLAcFIfsXQIq3p89uPZUW9gFl0dF18hGL2xelW6Ph8ViIQBaK0No7QOidM-SlgC25G0FKCegIZWhMtSrlXYbEDi2RAuBf2QdZpBDumk-w1IH5ZFed5d5iLqXivpaDN8R0Pyyuuv4AFuT4JeEcAS3l9BrTK0FSWClbzCZUrFYnCV5FyDyl4NaliR4XDy0dG9Z6JCjgJDHjSQS9I1znkNT60VgN-V7UoqFUN0Q5FsTkfWSCeUBWwXEvQJCfgVabAtI6EVrdM1BWzSpMKakrV-ndsy8FmU7RHm8Hse0jkJGLWpIIss2x3ENoBgnZtINBDSrAHmoCOJK0PQNJBJ1jlb4LXHrjb6+NvV6wzbO3a86gFgw7UlGpNqhHmT2CkOkMaskjr3ctA9nlp3Ez2We-ahEg3TmXb7NdvgN3PvOIyReb6yo-SPS3GdP6yaC2XVER5Uymm7FmW0rJT8EheO2HaAV9ov27ORUh025sab0yZqzdmKHsiL0Bc04FczHKQTuoWfYNpbILzcCRpFXyAwmzNlTKjVsi4oYQvLeWuR+leGAnlNj1jOPHECFlPjcHEEIbIx3FOacM7kCzsuzDoc1NOXqeNNIjl8oNPSTI-tJpEj8ZNVmzu+me6Gb7nnAutHITGfWGZ-BlxLNsfci4ceDGjR3HgnQo1vqm27Tc93XuOdvODxtsuuICFbHQXtHSRk7lQsLX+ZFzYRpf7OeAShzDC1SEOEJMkYFBLfCsUEW9UDwR-kKLTcepBRjUFgIgCh5w6w6uBHSU1t+I1boyO2LfXJFpKuxNMZAaru7b42hVelQ0lopsKpm3qeb8rbAFAKEAA */
-  id: "The Cool Companion",
-  initial: 'off',
-  type: 'parallel',
-  context: {
-    temperature: 25,
-  },
-  states: {
-    Refrigirator: {
-      initial: "Off",
-      states: {
-        Off: {
-          on: {
+{
+  "id": "The Cool Companion",
+  "initial": "off",
+  "states": {
+    "Refrigirator": {
+      "initial": "Off",
+      "states": {
+        "Off": {
+          "on": {
             "Connect to Power": {
-              target: "On",
+              "target": "On"
             }
           }
         },
-
-        On: {
-          type: 'parallel',
-          states: {
-            Cooler: {
-              initial: "Cooling",
-              states: {
-                Cooling: {
-                  on: {
+        "On": {
+          "states": {
+            "Cooler": {
+              "initial": "Cooling",
+              "states": {
+                "Cooling": {
+                  "on": {
                     "Temperature Reached": {
-                      target: "Idle",
-                      cond: "temp equals target temp"
+                      "target": "Idle",
+                      "cond": "temp equals target temp"
                     },
-    
-                    "FAULT": "Fault",
-                    "Ice Accumulated": "Defrosting"
-                  }
-                },
-
-                Idle: {
-                  on: {
-                    "Temperature Too High": {
-                      target: "Cooling",
-                      cond: "temp >  target temp"
+                    "FAULT": {
+                      "target": "Fault"
+                    },
+                    "Ice Accumulated": {
+                      "target": "Defrosting"
                     }
                   }
                 },
-
-                Defrosting: {
-                  on: {
-                    "Ice Melted": "Cooling",
-                    "FAULT": "Fault"
+                "Idle": {
+                  "on": {
+                    "Temperature Too High": {
+                      "target": "Cooling",
+                      "cond": "temp >  target temp"
+                    }
                   }
                 },
-
-                Fault: {
-                  type: "final"
+                "Defrosting": {
+                  "on": {
+                    "Ice Melted": {
+                      "target": "Cooling"
+                    },
+                    "FAULT": {
+                      "target": "Fault"
+                    }
+                  }
+                },
+                "Fault": {
+                  "type": "final"
                 }
               }
             },
-
-            Freezer: {
-              states: {
-                Freezing: {
-                  on: {
-                    FAULT: "Fault",
-                    "Ice Accumulated": "Defrosting",
+            "Freezer": {
+              "initial": "Freezing",
+              "states": {
+                "Freezing": {
+                  "on": {
+                    "FAULT": {
+                      "target": "Fault"
+                    },
+                    "Ice Accumulated": {
+                      "target": "Defrosting"
+                    },
                     "Temperature Reached": {
-                      target: "Idle",
-                      cond: "temp equals target temp"
+                      "target": "Idle",
+                      "cond": "temp equals target temp"
                     }
                   }
                 },
-                Idle: {
-                  on: {
+                "Idle": {
+                  "on": {
                     "Temperature Too High": {
-                      target: "Freezing",
-                      cond: "temp > target temp"
+                      "target": "Freezing",
+                      "cond": "temp > target temp"
                     }
                   }
                 },
-                Defrosting: {
-                  on: {
-                    FAULT: "Fault",
-                    "Ice Melted": "Freezing"
+                "Defrosting": {
+                  "on": {
+                    "FAULT": {
+                      "target": "Fault"
+                    },
+                    "Ice Melted": {
+                      "target": "Freezing"
+                    }
                   }
                 },
-                Fault: {
-                  type: "final"
+                "Fault": {
+                  "type": "final"
                 }
-              },
-
-              initial: "Freezing"
+              }
             },
-
-            Lights: {
-              states: {
-                LIGHTS_OFF: {
-                  on: {
-                    "turn on lights": "LIGHTS_ON"
+            "Lights": {
+              "initial": "LIGHTS_OFF",
+              "states": {
+                "LIGHTS_OFF": {
+                  "on": {
+                    "turn on lights": {
+                      "target": "LIGHTS_ON"
+                    }
                   }
                 },
-
-                LIGHTS_ON: {
-                  on: {
-                    "Turn Off lights": "LIGHTS_OFF"
+                "LIGHTS_ON": {
+                  "on": {
+                    "Turn Off lights": {
+                      "target": "LIGHTS_OFF"
+                    }
                   }
                 }
-              },
-
-              initial: "LIGHTS_OFF"
+              }
             },
-
             "Sound Alarm": {
-              states: {
-                SOUND_OFF: {},
-
-                SOUND_ON: {
-                  after: {
-                    "500": "SOUND_OFF"
+              "initial": "SOUND_OFF",
+              "states": {
+                "SOUND_OFF": {},
+                "SOUND_ON": {
+                  "after": {
+                    "2000": {
+                      "target": "#The Cool Companion.Refrigirator.On.Sound Alarm.SOUND_OFF",
+                      "actions": [],
+                      "internal": false
+                    }
                   }
                 }
-              },
-
-              initial: "SOUND_OFF"
+              }
             }
-          }
+          },
+          "type": "parallel"
         }
       }
     },
-
-    Door: {
-      initial: 'closed',
-      states: {
-        closed: {
-          on: {
-            "Open Door": {
-              target: 'opened'
+    "Door": {
+      "initial": "closed",
+      "states": {
+        "closed": {
+          "after": {
+            "0": {
+              "target": "#The Cool Companion.Refrigirator.On.Sound Alarm.SOUND_OFF",
+              "actions": [],
+              "internal": false
             },
+            "1": {
+              "target": "#The Cool Companion.Refrigirator.On.Lights.LIGHTS_OFF",
+              "actions": [],
+              "internal": false
+            }
           },
-
-          after: {
-            "1": [
-              { type: "The Cool Companion.Refrigirator.On.Lights.LIGHTS_OFF" },
-              { type: "The Cool Companion.Refrigirator.On.Sound Alarm.SOUND_OFF" },
-            ]
+          "on": {
+            "Open Door": {
+              "target": "opened"
+            }
           }
         },
-
-        opened: {
-          on: {
-            "Close Door": {
-              target: 'closed'
-              }
+        "opened": {
+          "after": {
+            "1": {
+              "target": "#The Cool Companion.Refrigirator.On.Lights.LIGHTS_ON",
+              "actions": [],
+              "internal": false
+            },
+            "500": {
+              "target": "#The Cool Companion.Refrigirator.On.Sound Alarm.SOUND_ON",
+              "actions": [],
+              "internal": false
+            }
           },
-
-          after: {
-            "1": "#The Cool Companion.Refrigirator.On.Lights.LIGHTS_ON",
-            "500": "#The Cool Companion.Refrigirator.On.Sound Alarm.SOUND_ON"
+          "on": {
+            "Close Door": {
+              "target": "closed"
+            }
           }
         }
       }
     }
-  }
-});
+  },
+  "type": "parallel"
+}
--- something.js
+++ something.js
@@ -1,4 +1,5 @@
 const smartRefrigerator = Machine({
+    /** @xstate-layout N4IgpgJg5mDOIC5SwLYEMBOAXASmAZhgJYwZpYD2GAdAMYA2FskAxAPIAKAogHID6AETZscAbQAMAXUSgADkyJYiFAHYyQAD0TiANCACe2gL5G9qTLgLFS5KnUbMILAVwAqXAMKu+HHGwEAql4AyhLSSCDysIrKahFaCLoGxqYg5th4hCRgZJQ0DEysLu5efABiAIIBADKuYepRMarqCUmGiSZm6BlW2bl2FLJgKiwe1WzBXILCYlINCkrN8dp67eKdad2WWTZ51IPDdBQU9EQqUCxlOFxcAFpcOHyT1Z7uAvURjYtxoK2rKV0LJlrDlbDQDiojiczhdqgBJADiAAlXMEnlwXl4uO85p8FrEWitkh1UultiD+uChpDaMdTucWBVqhUcABZdGYt4fOT4pa-IlrDZk4F9MH7anUQhgMAALxho2EL0ez1e2O5kV5P00AoBmyBvV2AwlUtl8vhyNRHNVOPCPOi30JiX+JMBPR2oL2EMlGGlcoZTJZ7JVWJt83tBOWTuJ61JWxFhqph1OUAAFlhYAq2EqrSH1V8I-yo4LY-r3ZTxUmSGmM1cbvdlRjrXnNY62rrhQaPUbK6n04zmWyc1zcXamlq-tGhXHO+WvWh6JgUJns8Hh7aNeG+dqi+3p2WxXOFxgl7W7g8h2qRxux63nTHXeTRZ6JfPFyxzSi0avL+v81uJ8WD7xl2NCyBgFAQAArrQWCsmgKhoDAKDDFgozjJM0wiM2m7jjqLp6m6FJimBEHQbB8GIWAyEqKhAQcAIFTuD4fiBCE2E3pGbb4R2+57CRUEwXBCFIShdApmAtAANYwhw4ECX2vj+EE3gVAILihniOG3pOJaEU+dj8WRQmUdRWBiRJ0nnLJpEwRmimsd41ysmwABqP5hhxhZcfeBGPgm1CGYJFEiTR5lSTJclkRmARwheGmjg6nF3lOpZEXxkVBcJVGiZBRAsDFfBcAAGnCdRXn+uE7txe5pQZGXkVlpnULl1CwCmgyyDC1RELAqEuXCXAAOp8B4AQ4NcPDeB4bCTbwZW-i2SU6UBM7EfVxkhWZLVtR1XU9X1A3DcVHBwtcAjMUpbHlYtXnJbpfkgQF63BdloXbe1sidec3W9Sw-VDXw1weHC3ChNdWlLYBvnAeWgUNSZOVEK1H1fVAP2ocEFRueUWMiKVUxAyDXDsYlhYAMwAEziNQABsAAsFNk0zACcAAcdNkwA7AArM6ZM0wAjNQ4iCzT3OCyL4jMwLKV6f5cMba9W1Iz6tBEEMGbXK4Y38K4bBPEinAnTwCJ8PCwTzR5pPbt5ssPbDz2NYj1Cq+rcDUAARmgjhsCoHiQRgPo0R4qhYChLCroDnhEyTBY23dK28XVNnw5tzUqxJbuwC7mdDAIYBYGgRD0Jrbg63wetR8DoOx-+eE+TxtWgY7CNvUjYe0CmKhELQ85lGgkH0KhWvl5XwSGxwxum+bluaZ58fLdDq17B3Xc933A9D2hExTEIWHg-PAG7ql+k0Kv3e9-Q-eD2ZEA9bIC76NfW8Fd+8XXtbR-VSf-nn+vV+b2VvlWKxVSq10qrbe6MMxR-0vs-ZWkpAE8AoEofA69vhoS4CyPgPA2CuDhGUOEHhGJwhmuAx0TNWbUAZuIAWZNuZ0zZuIOmNNmbOgFsw6hdNuEU05ow8QjMOasxMKkFQEE4DqEbqfK2ccEgAFoBbMwps6ORZNGHUGZtzAWdMBbczUQLDhFM6Z22gXsAojgZF1yqjTIxJjl7di1BVR0ci6asxUazTm1CPEeJsQLHmNNOYU25nYpOiYaR0hhJYiBzoKas2EVA+xYTvS+kiXPT+eEGYUy8czQWFNmb0NZhTRmISm4VkhMmasUTtLtG5uLDRnMlFBI8QLemLCSmnzKdQV8x4qmQ0QKwoWLCPHczyZzAJrMtHtPli3TavTCwKO4SoimNjqE03oTTVmOSxbM3EJzKZj0FYvSap3CyEUU7wDSbIjJrMG41Q6Ycp2bc5nbjkTklRfi6b1O5oEoJSjdmbL2Qk0JT0U6Kyau9Xa319rPK-hzCm+yHagqOc7V2GsYWIBcW44kciPFeM5j4imfjxaBOCUC0pDzW4INRe7L2Ps-YByDlgEONEULoqqmTdmCK1pIseVS3O7tqX50LsXC5CUrlVS0VQ1xOSBZ5IKUU4xZL7kzKVunagsCN43zZZi50nNxDU2Zg0op3NmmtPiYnUpGqAFasuVY15NN3GeNcfilhXM6ZaL8fCpVv8JJrzgYA6gd9YAPzQE-QBbKuLfJplylevqL6aqHunbVrj2E0w0eLE1BjdE-LFtG71j0rXwLVfgJBKCiBoN7tbJxfSEAjPiSYIAA */
     id: 'smartRefrigerator',
     initial: 'closed',
     context: {
@@ -18,6 +19,7 @@ const smartRefrigerator = Machine({
           DETECT_FAULT: 'technicalFault.displayFault'
         }
       },
+
       open: {
         on: {
           CLOSE_DOOR: 'closed'
@@ -54,6 +56,7 @@ const smartRefrigerator = Machine({
           }
         }
       },
+
       productManagement: {
         on: {
           CLOSE_DOOR: 'closed',
@@ -135,6 +138,7 @@ const smartRefrigerator = Machine({
           }
         }
       },
+
       technicalFault: {
            on: {
           CLOSE_DOOR: 'closed'
@@ -165,6 +169,6 @@ const smartRefrigerator = Machine({
       }
     }
       }
-  }
+    }
   })
   
